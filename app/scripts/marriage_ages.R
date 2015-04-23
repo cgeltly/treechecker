210bbcd9c44e1fe26da90a7b6f1529bd515041fd
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2015-04-23 08:53:22 +0200

    First version of marriage age R script

--- app/scripts/marriage_ages.R
+++ app/scripts/marriage_ages.R
@@ -11,18 +11,18 @@ con <- dbConnect(RMySQL::MySQL(),
 	port=3306)
 
 # Read the lifespans
-d <- dbReadTable(conn = con, name = 'lifespans')
+d <- dbReadTable(conn = con, name = 'marriage_ages')
 d <- subset(d, gedcom_id == args[1])
 
 # Update birth column; take the year and set it as numeric
 d <- transform(d, 
-	birth = as.numeric(substr(d$birth, 0, 4)), 
-	sex = factor(d$sex))
+	indi_birth = as.numeric(substr(d$indi_birth, 0, 4)), 
+	indi_sex = factor(d$indi_sex))
 
 # Plot the linear model as a trendline
-p <- qplot(birth, lifespan, data=d, color=sex)
+p <- qplot(indi_birth, marriage_age, data=d, color=indi_sex)
 p <- p + geom_smooth(method = "lm")
 
 # Save the result as .svg
-filename <- paste("lifespan", args[1], ".svg", sep="")
+filename <- paste("marriages_age", args[1], ".svg", sep="")
 ggsave(file=filename, plot=p, width=10, height=6)
